e4abb385fd2a94868888395fe056c7025c45ac3a
Aaron Bushnell <aaron.bushnell@gmail.com>
2017-08-03 21:14:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Docker LAMP
+Linux + Apache + MariaDB (MySQL) + PHP 5 on Docker Compose. Mod_rewrite enabled by default.
+
+## Instructions
+
+Enter the following command to start your containers:
+```bash
+$ docker-compose up -d
+```
+
+To stop them, use this:
+```bash
+$ docker-compose down
+```
---
+++ app/index.php
@@ -0,0 +1,13 @@
+<?php
+  $servername = "db";
+  $username = "admin";
+  $password = "test";
+  $dbname = "database";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  } else {
+    echo 'Successfully connected to database!';
+  }
